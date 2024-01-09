0465c31ac094d6fec0f64a01c070de44add00436
Henrique Klein Raymundo <henriqueklein231@gmail.com>
2024-01-09 12:15:56 -0300

    Delete src directory

--- src/app/(pages)/page.tsx
+++
@@ -1,8 +0,0 @@
-import { fetchProducts } from "@/lib/stripe";
-import { ProductsSlides } from "./components/ProductsSlides";
-
-export default async function Home() {
-    const products = await fetchProducts()
-
-    return <ProductsSlides products={products} />
-}
